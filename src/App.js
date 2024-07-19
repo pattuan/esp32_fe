3bfb69a0de79e9f4b64526a0aa9066d99e79678b
tuan <tonytuanpat2601@gmail.com>
2024-07-19 11:32:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppDEMO from './component/ToggleButton';
 import Login from './login';
 import Form from './form';
@@ -8,14 +8,14 @@ import Camera from './camera';
 function App() {
   return (
     <div className="App">
-      <Router basename="/esp32_fe">
+      <BrowserRouter basename="/esp32_fe">
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route exact path="/" element={<Login />}/>
           <Route path="/AppDEMO" element={<AppDEMO />} />
           <Route path="/form" element={<Form />} />
           <Route path="/camera" element={<Camera />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
