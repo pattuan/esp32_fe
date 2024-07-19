ae5a998a157377fd8acd22cfcadd2ce126cabef4
tuan <tonytuanpat2601@gmail.com>
2024-07-19 12:32:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,14 +8,12 @@ import Camera from './camera';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/esp32_fe">
         <Routes>
-          <Route exact path="/" element={<Login />}/>
+          <Route path="/" element={<Login />}/>
           <Route path="/AppDEMO" element={<AppDEMO />} />
           <Route path="/form" element={<Form />} />
           <Route path="/camera" element={<Camera />} />
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter} from 'react-router-dom'
+// import { CartProvider } from "react-use-cart";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
