0de6196564f2ecc44b071fcda2cd840a68b034ed
tuan <tonytuanpat2601@gmail.com>
2024-07-19 11:55:20 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import AppDEMO from './component/ToggleButton';
 import Login from './login';
 import Form from './form';
@@ -8,14 +8,14 @@ import Camera from './camera';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/esp32_fe">
+      <HashRouter basename="/esp32_fe">
         <Routes>
           <Route exact path="/" element={<Login />}/>
           <Route path="/AppDEMO" element={<AppDEMO />} />
           <Route path="/form" element={<Form />} />
           <Route path="/camera" element={<Camera />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
